a83440cebad070bf1238df3e5e0704fedab50d9a
shreeram26 <61724305+shreeram26@users.noreply.github.com>
2020-03-16 10:53:20 +0530

    Update prac6.py

--- prac6.py
+++ prac6.py
@@ -1,14 +1,13 @@
 from tkinter import*
 root=Tk()
-v=IntVar()
-v.set(0)
-lang_list=[("Python",0), ("WP",1), ("jQuery",2),
-           ("Javascript",3) ]
+var=IntVar()
+var.set(0)
+lang_list=[("Python",0), ("WP",1), ("jQuery",2),("Javascript",3)]
 def ShowChoice() :
-    print ("You selected choice number: ", v.get() )
+    print ("You selected choice number: ", var.get() )
 l=Label(root, text="Select your favourite programming language: ")
 l.pack()
 for txt,val in lang_list:
-    r=Radiobutton(root, text=txt, variable=v,
+    r=Radiobutton(root, text=txt, variable=var,
                   command=ShowChoice, value=val)
     r.pack()
